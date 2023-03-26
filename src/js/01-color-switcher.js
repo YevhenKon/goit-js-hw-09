e5019e64af79210c609ec53d1aaebff9731fa3ef
Yevhen Kontselenko <105706808+YevhenKon@users.noreply.github.com>
2023-03-26 23:35:43 +0300

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+startBtn.addEventListener("click", () => timerId = setInterval(changeColor, 1000));
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    startBtn.disabled = false
+    stopBtn.disabled = true        
+});
+
+function changeColor() {
+    document.body.style.backgroundColor = getRandomHexColor()
+    startBtn.disabled = true
+    stopBtn.disabled = false
+}
